68ebbd55a3845813f0e8313b74950d9fff3a48ef
João Ramajo <joaoramajo744@gmail.com>
2025-12-10 22:03:26 -0300

    chore(UserDto): adiciona docblock para documentação da classe

--- src/Domain/Dtos/UserDto.php
+++ src/Domain/Dtos/UserDto.php
@@ -1,22 +1,27 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace App\Domain\Dtos;
 
 use DateTimeImmutable;
 
+/**
+ * @param string $name
+ * @param string $email
+ * @param string $password
+ * @param ?int $id
+ * @param ?DateTimeImmutable $email_verified_at
+ * @param ?DateTimeImmutable $created_at
+ * @param ?DateTimeImmutable $updated_at
+ */
 class UserDto
 {
     public function __construct(
-        public readonly ?int $id = null,
         public readonly string $name,
         public readonly string $email,
         public readonly string $password,
-        public readonly ?DateTimeImmutable $email_verified_at,
-        public readonly ?DateTimeImmutable $created_at,
-        public readonly ?DateTimeImmutable $verified_at,
-    )
-    {
-    }
+        public readonly ?int $id = null,
+        public readonly ?DateTimeImmutable $email_verified_at = null,
+        public readonly ?DateTimeImmutable $created_at = null,
+        public readonly ?DateTimeImmutable $updated_at = null,
+    ) {}
 }
