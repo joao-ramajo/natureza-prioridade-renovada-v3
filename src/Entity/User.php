bf2361d9bce60dd26f7a760832067b1742525ef6
João Ramajo <joaoramajo744@gmail.com>
2025-12-10 21:05:00 -0300

    chore(phpstan): adiciona phpstan para análise estática de código e configuração do arquivo phpstan.neon para projetos symfony

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
